f0e9b150ae9676b528b262cc10aedc3783119444
BoeyTheBlade <boey@lyse.net>
2023-11-08 13:55:13 +0100

    9th

--- routes/pictures.js
+++ routes/pictures.js
@@ -12,10 +12,10 @@ router.get('/', requiresAuth(), async function(req, res, next) {
   var params = {
     Bucket: process.env.CYCLIC_BUCKET_NAME,
     Delimiter: '/',
-    Prefix: 'public/'
+    Prefix: req.oidc.user.email + '/'
   };
   var allObjects = await s3.listObjects(params).promise();
-  var keys = allObjects?.Contents.map( x=> x.Key).slice(0,3);
+  var keys = allObjects?.Contents.map( x=> x.Key)
   const pictures = await Promise.all(keys.map(async (key) => {
     let my_file = await s3.getObject({
       Bucket: process.env.CYCLIC_BUCKET_NAME,
@@ -34,9 +34,19 @@ router.get('/', requiresAuth(), async function(req, res, next) {
   res.render('pictures', { pictures: pictures});
 }); */
 
-router.get('/:pictureName', function(req, res, next) {
-  res.render('pictureDetails', { picture: req.params.pictureName});
+router.get('/:pictureName', requiresAuth(), async function(req, res,
+  next) {
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + req.params.pictureName,
+  }).promise();
+  const picture = {
+    src: Buffer.from(my_file.Body).toString('base64'),
+    name: req.params.pictureName
+  }
+  res.render('pictureDetails', { picture: picture});
   });
+  
 
   router.post('/', requiresAuth(), async function(req, res, next) {
     const file = req.files.file;
@@ -44,7 +54,7 @@ router.get('/:pictureName', function(req, res, next) {
     await s3.putObject({
       Body: file.data,
       Bucket: process.env.CYCLIC_BUCKET_NAME,
-      Key: "public/" + file.name,
+      Key: req.oidc.user.email + "/" + file.name,
     }).promise()
     res.end();
   });
