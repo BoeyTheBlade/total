b99cf062bd04aff8c0bed43563ac16cff27b9033
BoeyTheBlade <boey@lyse.net>
2023-11-09 12:17:38 +0100

    11th

--- routes/pictures.js
+++ routes/pictures.js
@@ -37,15 +37,16 @@ router.get('/', requiresAuth(), async function(req, res, next) {
 router.get('/:pictureName', requiresAuth(), async function(req, res,
   next) {
   let my_file = await s3.getObject({
-    Bucket: process.env.CYCLIC_BUCKET_NAME,
-    Key: "public/" + req.params.pictureName,
-  }).promise();
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: req.oidc.user.email + '/'+ req.params.pictureName,
+    }).promise();
   const picture = {
-    src: Buffer.from(my_file.Body).toString('base64'),
-    name: req.params.pictureName
-  }
+      src: Buffer.from(my_file.Body).toString('base64'),
+      name: req.params.pictureName
+    }
   res.render('pictureDetails', { picture: picture});
   });
+  
   
 
   router.post('/', requiresAuth(), async function(req, res, next) {
